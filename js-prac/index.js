3a8ff8b8ec0289581948b34470b82e7789f32e28
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-08-20 10:15:19 +0300

    updated

--- js-prac/index.js
+++ js-prac/index.js
@@ -1,17 +1,44 @@
-const disp=document.querySelector(".display")
+// const disp=document.querySelector(".display")
 
 
 
-localStorage.setItem("message","hello world !!");
+// localStorage.setItem("message","hello world !!");
 
-const result = localStorage.getItem("message");
+// const result = localStorage.getItem("message");
 
-// console.log(result);
+// // console.log(result);
 
-const mess=document.createElement("h1");
-mess.innerHTML=result;
+// const mess=document.createElement("h1");
+// mess.innerHTML=result;
 
-disp.appendChild(mess);
+// disp.appendChild(mess);
+
+//Rose – unit price: 8 , quantity: 70 , value: 560
+// Lily – unit price: 10 , quantity: 50 , value: 500
+// Tulip – unit price: 2 , quantity: 120 , value: 240
+// Total: 1300
+
+
+//price
+let rose=8;
+let lily=10;
+let tulip=2;
+
+
+
+function totalPrice(flower,quantity){
+let total =flower*quantity;
+
+return total;
+
+}
+
+
+console.log("This is the total price for roses: ",totalPrice(rose,70));
+console.log("This is the total price for lily: ",totalPrice(lily,50));
+console.log("This is the total price for tulip: ",totalPrice(tulip,120));
+console.log("Totals = ",totalPrice(rose,70)+totalPrice(lily,50)+totalPrice(tulip,120));
+
 
 
 
